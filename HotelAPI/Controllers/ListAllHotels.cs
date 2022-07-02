7694596a63692b6064ad2f7ddd5e596df8b8d796
moizsabun <moizzoaib786@gmail.com>
2022-07-02 21:38:00 +0500

    Added listAllHotels controller anf Middlewares in program.cs file

---
+++ HotelAPI/Controllers/ListAllHotels.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListAllHotels : ControllerBase
+    {
+        [HttpGet]
+        public string get()
+        {
+            return "Welcome to Hotel API.";
+        }
+    }
+}
--- HotelAPI/Program.cs
+++ HotelAPI/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
+builder.Services    .AddControllers(options => options.EnableEndpointRouting = false);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseCors("CorsPolicy");
 
+app.UseAuthentication();
+
+app.UseMvc();
 app.Run();
