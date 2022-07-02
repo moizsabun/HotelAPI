8bafa427fbedd48417c9cd41308915015cba3a8c
moizsabun <moizzoaib786@gmail.com>
2022-07-02 21:31:30 +0500

    Add project files.

---
+++ HotelAPI.DA/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelAPI.DA
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+    }
+}
